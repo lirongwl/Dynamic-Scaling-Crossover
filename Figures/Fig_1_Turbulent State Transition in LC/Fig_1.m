6b06ac80db01aadf6fb569233ab30bb735f90f32
lirongwl <lirong13@westlake.edu.cn>
2025-08-19 14:08:34 +0800

    Add files via upload

---
+++ Figures/Fig_1_Turbulent State Transition in LC/Fig_1.m
@@ -0,0 +1,40 @@
+currentFolder = fileparts(mfilename('fullpath'));
+figure('Position', [0, 0, 1800, 1000]);
+rhoFolder = fullfile(currentFolder, '1 rho');
+psFolder  = fullfile(currentFolder, '2 Ps');
+
+clear; clf; clc;
+
+cd('1_rho/Fig_1e')
+subplot(2,3,1)
+Fig_1a
+text(-0.2, 1.05, 'a', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
+
+hold on
+subplot(2,3,2)
+Fig_1c
+text(-0.2, 1.05, 'b', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
+
+hold on 
+subplot(2,3,3)
+Fig_1e
+text(-0.2, 1.05, 'c', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
+
+cd .. 
+cd ..
+
+cd('2_Ps/Fig_1f')
+
+subplot(2,3,4)
+Fig_1b
+text(-0.2, 1.05, 'd', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
+
+hold on
+subplot(2,3,5)
+Fig_1d
+text(-0.2, 1.05, 'e', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
+
+hold on 
+subplot(2,3,6)
+Fig_1f
+text(-0.2, 1.05, 'f', 'FontName', 'Arial', 'FontSize', 16, 'FontWeight', 'bold', 'Units', 'normalized', 'HorizontalAlignment', 'left');
